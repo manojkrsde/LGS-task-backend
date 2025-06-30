6fc7df9967c7a07d3e6d15abff86157ad51f4865
Manoj Kumar <manojkumar.ibmce@gmail.com>
2025-06-30 17:33:36 +0530

    FIX: update to ES6 moduling

--- src/models/index.js
+++ src/models/index.js
@@ -1,37 +1,50 @@
-'use strict';
+import fs from "fs";
+import path from "path";
+import { fileURLToPath, pathToFileURL } from "url";
+import Sequelize from "sequelize";
+import configObj from "../config/database.js"; // Your database config as ES module
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const env = process.env.NODE_ENV || "development";
+const config = configObj[env];
+
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
 }
 
-fs
+// Read all model files dynamically
+const files = fs
   .readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
+  .filter(
+    (file) =>
+      file.indexOf(".") !== 0 &&
       file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
+      file.slice(-3) === ".js" &&
+      !file.includes(".test.js")
+  );
 
-Object.keys(db).forEach(modelName => {
+for (const file of files) {
+  const modelPath = pathToFileURL(path.join(__dirname, file)).href;
+  const { default: modelFn } = await import(modelPath);
+  const model = modelFn(sequelize, Sequelize.DataTypes);
+  db[model.name] = model;
+}
+
+// Setup associations
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
@@ -40,4 +53,4 @@ Object.keys(db).forEach(modelName => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-module.exports = db;
+export default db;
