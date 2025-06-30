d5e39a14dadaa89791f748a58fd8a44d27214218
Manoj Kumar <manojkumar.ibmce@gmail.com>
2025-06-30 14:53:07 +0530

    FIX: added books model and migration file

---
+++ src/migrations/20250630090625-create-books.js
@@ -0,0 +1,44 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+export default {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("books", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.TEXT,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("books");
+  },
+};
---
+++ src/models/books.js
@@ -0,0 +1,33 @@
+import { Model } from "sequelize";
+
+export default (sequelize, DataTypes) => {
+  class Book extends Model {
+    static associate(models) {
+    }
+  }
+
+  Book.init(
+    {
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Book",
+      tableName: "books",
+      underscored: true,
+      timestamps: true,
+    }
+  );
+
+  return Book;
+};
